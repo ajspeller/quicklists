3e0c82c623e6bbf912a8683b288aab2d9368f354
AJ Speller <ajspeller@gmail.com>
2022-01-19 10:39:53 -0500

    Add packages for storage, statusbar, splashscreen, sqlite and image assets.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+import { SplashScreen } from '@capacitor/splash-screen';
+import { StatusBar } from '@capacitor/status-bar';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.initializeApp();
+  }
+
+  async initializeApp() {
+    SplashScreen.hide().catch((err) => {
+      console.warn(`Ummm... Something went wrong with the SplashScreen`);
+      console.warn(err);
+    });
+
+    StatusBar.setBackgroundColor({ color: '#2dd36f' }).catch((err) => {
+      console.warn(`Ummm... Something went wrong with the StatusBar`);
+      console.warn(err);
+    });
+  }
 }
